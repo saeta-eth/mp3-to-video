bd75858e1bc88440d55a9971d14dbf7355929e92
slorenzo <sebastianlorenzo@gmail.com>
2016-10-03 19:57:04 -0300

    add new ffmpeg param

--- src/convert/convert.js
+++ src/convert/convert.js
@@ -1,5 +1,6 @@
 import fileExists from 'file-exists';
 import FFmpeg from 'fluent-ffmpeg';
+import fs from 'fs';
 
 /* eslint class-methods-use-this:
 ["error", {"exceptMethods": ["checkTypes", "isString", "getNameOfPath"] }] */
@@ -14,7 +15,7 @@ class Convert {
     this.path = path;
     this.ext = ext;
     this.image = image;
-    this.output = `${this.getNameOfPath(this.path)}.${this.ext}`;
+    this.output = this.fillWithNewExt(this.path, this.ext);
   }
 
   /**
@@ -75,12 +76,12 @@ class Convert {
   }
 
   /**
-    * It return a basename of path
+    * It return a path with output extension
     * @param {string} Path of mp3
+    * @param {string} extension
   */
-  getNameOfPath(path) {
-    const baseName = /^.+\.([^.]+)$/.exec(path);
-    return baseName == null ? '' : baseName[0];
+  fillWithNewExt(path, ext) {
+    return path.substr(0, path.lastIndexOf(".")) + '.' +ext;
   }
 
   /**
@@ -90,7 +91,7 @@ class Convert {
     * @param {string} output path to output file (eg. /path/to/output.mp4)
     * @param {convert~requestCallback} callback
   */
-  static convert(audio, image, output, callback) {
+  convert(audio, image, output, callback) {
     const Command = new FFmpeg();
     Command
       .input(image)
@@ -101,12 +102,11 @@ class Convert {
       .output(output)
       .outputOptions([
         '-c:v libx264',
-        '-c:v libx264',
+        '-crf 18',
         '-c:a aac',
         '-strict experimental',
         '-b:a 192k',
-        '-pix_fmt yuv420p',
-        '-shortest',
+        '-shortest'
       ])
       .on('error', (err) => {
         callback(err);
@@ -120,38 +120,25 @@ class Convert {
   /**
     * Set default error if parameter is missing
   */
-  static throwIfMissing() {
+  throwIfMissing() {
     throw new Error('Missing parameter.');
   }
 
   /**
-    * It is a callback from convert function
-    * @param {object} Can be null or error object
-    * @param {boolean} Can be null or true
-    * @param {hasFinished~requestCallback} callback
-  */
-  static hasFinished(err, success) {
-    if (err) {
-      throw new Error(`Something has wrong: ${err}`);
-    } else {
-      return success;
-    }
-  }
-  /**
     * It is a module initialize
     * @param {init~requestCallback} callback
   */
-  init(callback) {
-    if (!fileExists(this.path)) throw new Error('MP3 path is wrong.');
-    if (!fileExists(this.image)) throw new Error('Image path is wrong.');
-    const flagOK = this.convert(this.path, this.image, this.output, this.hasFinished);
-    if (flagOK) {
-      callback({
+  init(callback) {  
+    if (!fileExists(this.path)) callback(new Error('MP3 path is wrong.'));
+    if (!fileExists(this.image)) callback(new Error('Image path is wrong.'));
+    this.convert(this.path, this.image, this.output, (err, success) => {
+      if(err) callback(err);
+      callback(null, {
         status: 'OK',
         message: 'Everything OK',
         videoPath: this.output,
       });
-    }
+    });
   }
 }
 
